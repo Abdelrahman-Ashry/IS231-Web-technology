eb9bec53f1f2f7aa1933f10c3a6554ef2e912416
amalhk2006-cell <amal.h.k2006@gmail.com>
2026-04-12 00:41:45 +0200

    Add files via upload

---
+++ Add Book.js
@@ -0,0 +1,31 @@
+function validateForm() {
+    let title = document.querySelector("input[placeholder='Enter Book Title']").value;
+    let year = document.querySelector("input[placeholder='Enter Publication Year']").value;
+
+    if (title === "") {
+        alert("Please enter book title");
+        return false;
+    }
+
+    if (year < 1000 || year > 2026) {
+        alert("Enter a valid year");
+        return false;
+    }
+
+    return true;
+}
+function saveBook() {
+    let title = document.querySelector("input[placeholder='Enter Book Title']").value;
+
+    localStorage.setItem("bookTitle", title);
+
+    alert("Book Saved!");
+}
+function clearForm() {
+    if (confirm("Are you sure?")) {
+        document.querySelectorAll("input, textarea").forEach(el => el.value = "");
+    }
+}
+function showMessage() {
+    document.getElementById("msg").innerText = "✅ Book Added Successfully!";
+}
